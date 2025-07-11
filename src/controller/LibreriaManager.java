4530267b3b9f123f0bec5c70c0b2e18689c59677
Pietro Vincenzi <pietro.vincenzi03@gmail.com>
2025-07-11 20:36:42 +0100

    +libreria manager (wip)

---
+++ src/controller/LibreriaManager.java
@@ -0,0 +1,53 @@
+package controller;
+
+import model.Libreria;
+import model.Libro;
+import persistance.LibreriaDAO;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibreriaManager {
+    private final Libreria libreria;
+    private final LibreriaDAO dao;
+
+    public LibreriaManager(Libreria libreria, LibreriaDAO dao) {
+        this.libreria = libreria;
+        this.dao = dao;
+    }
+
+    public void salva() throws IOException {
+        dao.salvaLibreria(libreria.getLibri());
+    }
+
+    public void carica() throws IOException {
+        List<Libro> caricati = dao.caricaLibreria();
+        libreria.setLibri(caricati);
+    }
+
+
+    public boolean addLibro(Libro libro) throws IOException {
+        libreria.addLibro(libro);
+        salva();
+        return true;
+    }
+
+    public boolean removeLibro(Libro libro) throws IOException {
+        libreria.removeLibro(libro);
+        salva();
+        return true;
+    }
+
+    public boolean modificaLibro(Libro libro) throws IOException {
+        libreria.modificaLibro(libro);
+        salva();
+        return true;
+    }
+
+    public ArrayList<Libro> getiListaLibri() throws IOException {
+        return libreria.getLibri();
+    }
+
+
+}
