8b22fdd3cac2d63aefb912d29bd36befc882afef
Pietro Vincenzi <pietro.vincenzi03@gmail.com>
2025-07-12 15:43:45 +0100

    +Predisposizione per observer

--- src/controller/LibreriaManager.java
+++ src/controller/LibreriaManager.java
@@ -3,12 +3,13 @@ package controller;
 import model.Libreria;
 import model.Libro;
 import persistance.LibreriaDAO;
+import sun.jvm.hotspot.utilities.Observable;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LibreriaManager {
+public class LibreriaManager extends Observable {
     private final Libreria libreria;
     private final LibreriaDAO dao;
 
@@ -30,24 +31,29 @@ public class LibreriaManager {
     public boolean addLibro(Libro libro) throws IOException {
         libreria.addLibro(libro);
         salva();
+        setChanged();
+        notifyObservers();
         return true;
     }
 
     public boolean removeLibro(Libro libro) throws IOException {
         libreria.removeLibro(libro);
         salva();
+        setChanged();
+        notifyObservers();
         return true;
     }
 
     public boolean modificaLibro(Libro libro) throws IOException {
         libreria.modificaLibro(libro);
         salva();
+        setChanged();
+        notifyObservers();
         return true;
     }
 
     public ArrayList<Libro> getiListaLibri() throws IOException {
         return libreria.getLibri();
     }
-
 
 }
