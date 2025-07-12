c16721c6c2398bbb77fcd28b41349d87cf322252
Pietro Vincenzi <pietro.vincenzi03@gmail.com>
2025-07-12 16:04:11 +0100

    +init, getInstance

--- src/controller/LibreriaManager.java
+++ src/controller/LibreriaManager.java
@@ -3,6 +3,7 @@ package controller;
 import model.Libreria;
 import model.Libro;
 import persistance.LibreriaDAO;
+import persistance.LibreriaJsonDAO;
 import sun.jvm.hotspot.utilities.Observable;
 
 import java.io.IOException;
@@ -12,11 +13,27 @@ import java.util.List;
 public class LibreriaManager extends Observable {
     private final Libreria libreria;
     private final LibreriaDAO dao;
+    private static LibreriaManager instance;
 
-    public LibreriaManager(Libreria libreria, LibreriaDAO dao) {
+
+    private LibreriaManager(Libreria libreria, LibreriaDAO dao) {
         this.libreria = libreria;
         this.dao = dao;
     }
+
+    public static void init(Libreria libreria, LibreriaDAO dao) {
+        if (instance == null) {
+            instance = new LibreriaManager(libreria, dao);
+        }
+    }
+
+    public static LibreriaManager getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("LibreriaManager non inizializzato!");
+        }
+        return instance;
+    }
+
 
     public void salva() throws IOException {
         dao.salvaLibreria(libreria.getLibri());
