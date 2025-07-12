9187d02ea7dff887de5358ef9aee425c41c85745
Pietro Vincenzi <pietro.vincenzi03@gmail.com>
2025-07-12 10:58:41 +0100

    +Filtro autore

---
+++ src/filter/FiltroAutore.java
@@ -0,0 +1,17 @@
+package filter;
+
+import model.Libro;
+
+public class FiltroAutore implements FiltroLibro {
+   private String autore;
+
+    public FiltroAutore(String autore) {
+        this.autore = autore;
+    }
+
+    @Override
+    public boolean filter(Libro libro) {
+        return libro.getAutore() != null && libro.getAutore().toLowerCase().contains(autore);
+    }
+}
+
